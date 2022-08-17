7bbf49108a1edeb31ee0e21e64bf3f9e88828e42
srini <finzly>
2022-08-17 22:10:22 +0530

    FirstCommit

---
+++ neww/src/collections/LinkedHashSets.java
@@ -0,0 +1,13 @@
+package collections;
+
+public class LinkedHashSets {
+	public static void main(String[] args) {
+		LinkedHashSet<Integer> lh =new LinkedHashSet<>();
+		lh.add(15);
+		lh.add(89);
+		lh.add(15);
+		lh.add(35);
+		System.out.println(lh);
+	}
+
+}
